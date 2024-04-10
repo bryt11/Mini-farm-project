82122b555175c8a52f4eb90272ecc257c8d14de4
bryt11 <brightudodigwe@gmail.com>
2024-04-10 11:16:48 +0200

    First Commit
    First Date 10.4.2024

---
+++ index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+////////////////////////////////////////////////
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
+const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  const pathName = req.url;
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(' ');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObject[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+
+    // Not found (error)
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Say hello to mr error lol',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 3000');
+});
